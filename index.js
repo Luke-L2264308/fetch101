aeee20d0e70c20e8a06c2df4220dda704b60432e
Luke Linington <lukelinington@gmail.com>
2026-02-11 16:36:33 +0000

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ async function showMessage(elem, url) {
   const content = await response.text();
   elem.textContent = content;
 }
+
+async function showList(elem,url){
+  const response = await fetch(url);
+  
+  if (response.ok){
+    const content = await response.json();
+    for (let i = 0;i<content.length;i++){
+      const li = document.createElement('li');
+      li.textContent = content[i];
+      elem.append(li);
+  }
+    
+  }
+  
+  else
+    return reportError('No data');
+  
+  
+}
+function filler(list,candidates){
+  
+  for (let i = 0; i < candidates.length; i++) {
+    
+    const li = document.createElement('li');
+    li.textContent = candidates[i];
+    list.append(li);
+  }
+  return list;
+}
